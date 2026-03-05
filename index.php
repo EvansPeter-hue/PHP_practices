e0b5a59d18dd6741be06ea2d0034c883353141a6
Your Name <you@example.com>
2026-03-05 09:09:53 +0200

    Changes

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Evans Peter PHP</title>
+</head>
+<body>
+
+<h1>Evans Peter PHP</h1>
+<?php
+// Compute SHA256 hash of your name
+print "<p>The SHA256 hash of 'Evans Peter' is:</p>";
+print "<p>";
+print hash('sha256', 'Evans Peter');
+print "</p>";
+?>
+
+<h3>ASCII art:</h3>
+
+<pre>
+    ************
+     *        *
+      *      *
+       *    *
+         *
+    *
+    *
+    ************
+    *
+    ************
+</pre>
+
+</body>
+</html>
